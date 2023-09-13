bd1b65ac0cbe020c55b24fb7c8e98305b1e77f85
satya8096 <21MH5A0326@acoe.edu.in>
2023-09-14 00:51:24 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -13,3 +13,82 @@ Bars.onclick = ()=>{
 xmark.onclick = ()=>{
     Nav.style.height = "0rem"
 }
+
+
+// Top Button Script
+let mybutton = document.getElementById("top-btn1");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+// FAQ Script
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      this.style.color = "#444"
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      this.style.color = "blue"
+    } 
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
